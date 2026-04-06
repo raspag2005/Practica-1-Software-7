569184ddab13624e2ffb378867812834938821c8
Rubén Spagnuolo <raspag2005@gmail.com>
2026-04-06 14:29:57 -0500

    Creación de repositorio

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica sumativa</title>
+</head>
+<body>
+    <h1>Elegir Problema</h1><br>
+    <a href="prob1.php">Problema 1</a><br>
+    <a href="prob2.php">Problema 2</a><br>
+    <a href="prob3.php">Problema 3</a><br>
+</body>
+</html>
---
+++ prob2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Problema 2</title>
+</head>
+<body>
+    <h1>Problema 2</h1>
+    <?php
+        $numero1 = 10;
+        $numero2 = 4;
+        $resultado = $numero1 - $numero2;
+        echo $resultado;
+    ?>
+    <br> <a href="index.php">Regresar al índice</a>
+</body>
+</html>
