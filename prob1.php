6df8353e1ab02f0894719b5dac5dc3572240b014
Rubén Spagnuolo <raspag2005@gmail.com>
2026-04-06 15:00:45 -0500

    Finalización de práctica

--- prob1.php
+++ prob1.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Problema 1</title>
-</head>
-<body>
-    <h1>Problema 1</h1>
-    <?php
-        $numero1 = 5;
-        $numero2 = 7;
-        $resultado = $numero1 + $numero2;
-        echo $resultado;
-    ?>
-    <br> <a href="index.php">Regresar al índice</a>
-</body>
-</html>
+<?php 
+$numero1 = 5;
+$numero2 = 3;
+$resultado = $numero1 + $numero2;
+echo $resultado; 
+?>
--- prob3.php
+++ prob3.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Problema 3</title>
-</head>
-<body>
-    <h1>Problema 3</h1>
-    <?php
-        $numero1 = 7;
-        $numero2 = 3;
-        $resultado = $numero1 * $numero2;
-        echo $resultado;
-    ?>
-    <br> <a href="index.php">Regresar al índice</a>
-</body>
-</html>
+<?php 
+$numero1 = 7;
+$numero2 = 3;
+$resultado = $numero1 * $numero2;
+echo $resultado; 
+?>
